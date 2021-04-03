1ed5c244b1e4294250b7cd1e28e0f13a33346261
Jey <ash_88kj@yahoo.com>
2021-04-04 00:20:27 +0800

    migration and seeder

--- database/migrations/2021_04_03_155603_create_orders_details_table.php
+++ database/migrations/2021_04_03_155603_create_orders_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOrdersDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders_details', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('user_role');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('order_id');
+            $table->string('product_id');
+            $table->string('qty')->comment('Quantity of each product');
+            $table->string('price')->comment('Total Price per Quantity');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders_details');
     }
 }
